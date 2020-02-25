a1a1473b7a4b1734908e6368e3b506e2e7ac3367
Matthias Christenson <matchristenson@gmail.com>
2020-02-25 15:26:10 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dreye',
+      version='0.1',
+      description='Dreye',
+      author='Matthias Christenson',
+      author_email='gucky@gucky.eu',
+      install_requires=['numpy'],
+      packages=['dreye']
+)
