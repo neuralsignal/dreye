53c8b0634ecc504dd13aa8aadeb2c9168aee6c3a
gucky92 <gucky@gucky.eu>
2020-03-11 18:39:26 -0400

    change serializer

--- dreye/io/json.py
+++ dreye/io/json.py
@@ -87,51 +87,38 @@ def serializer(obj):
     """
 
     if isinstance(obj, np.ndarray):
-        obj = ARRAY_PREFIX + json.dumps(obj.tolist())
+        obj = {ARRAY_PREFIX: obj.tolist()}
     elif isinstance(obj, pd.Series):
-        obj = (
-            SERIES_PREFIX,
-            obj.to_dict()
-        )
+        obj = {SERIES_PREFIX: obj.to_dict()}
     elif isinstance(obj, pd.DataFrame):
-        obj = (
-            DFRAME_PREFIX,
-            obj.to_dict()
-        )
+        obj = {DFRAME_PREFIX: obj.to_dict()}
     elif isinstance(obj, np.dtype):
-        obj = DTYPE_PREFIX + str(obj)
+        obj = {DTYPE_PREFIX: str(obj)}
     elif isinstance(obj, UREG.Unit):
-        obj = PINT_PREFIX + str(obj)
+        obj = {PINT_PREFIX: str(obj)}
     elif isinstance(obj, UREG.Quantity):
         mag = obj.magnitude
         if isinstance(mag, np.ndarray):
             mag = mag.tolist()
-        obj = (
-            QUANT_PREFIX,
-            mag,
-            str(obj.units)
-        )
+        obj = {
+            QUANT_PREFIX: [mag, str(obj.units)]
+        }
     elif isinstance(obj, AbstractSequence):
-        # TODO checking of module
         cls = obj.__class__
-        obj = (
-            DR_PREFIX,
-            str(cloudpickle.dumps(cls)),
-            obj.to_dict()
-        )
+        obj = {
+            DR_PREFIX:
+            [str(cloudpickle.dumps(cls)), obj.to_dict()]
+        }
     elif hasattr(obj, 'to_dict') and hasattr(obj, 'from_dict'):
-        # TODO checking of module
         cls = obj.__class__
-        obj = (
-            DICTABLE_PREFIX,
-            str(cloudpickle.dumps(cls)),
-            obj.to_dict()
-        )
+        obj = {
+            DICTABLE_PREFIX:
+            [str(cloudpickle.dumps(cls)), obj.to_dict()]
+        }
     else:
-        obj = (
-            FUNC_PREFIX,
-            str(cloudpickle.dumps(obj))
-        )
+        obj = {
+            FUNC_PREFIX: str(cloudpickle.dumps(obj))
+        }
 
     return obj
 
@@ -140,39 +127,26 @@ def deserializer(obj):
     """deserializer of numpy, pandas objects and dreye objects etc.
     """
 
-    for key, ele in obj.items():
-        if isinstance(ele, str):
-            if ele.startswith(ARRAY_PREFIX):
-                ele = np.array(json.loads(ele[len(ARRAY_PREFIX):]))
-                obj[key] = ele
-            elif ele.startswith(DTYPE_PREFIX):
-                ele = np.dtype(ele[len(DTYPE_PREFIX):])
-                obj[key] = ele
-            elif ele.startswith(PINT_PREFIX):
-                ele = UREG(ele[len(PINT_PREFIX):]).units
-                obj[key] = ele
+    if len(obj) > 1:
+        return obj
 
-        elif isinstance(ele, (tuple, list)):
-            if len(ele) == 2:
-                if ele[0] == FUNC_PREFIX:
-                    obj[key] = pickle.loads(eval(ele[1]))
-                elif ele[0] == SERIES_PREFIX:
-                    ele = pd.Series(ele[1])
-                    obj[key] = ele
-                elif ele[0] == DFRAME_PREFIX:
-                    ele = pd.DataFrame(ele[1])
-                    obj[key] = ele
-            elif len(ele) == 3:
-                if ele[0] == QUANT_PREFIX:
-                    ele = ele[1] * UREG(ele[2]).units
-                    obj[key] = ele
-                elif ele[0] == DR_PREFIX:
-                    cls = pickle.loads(eval(ele[1]))
-                    ele = cls.from_dict(ele[2])
-                    obj[key] = ele
-                elif ele[0] == DICTABLE_PREFIX:
-                    cls = pickle.loads(eval(ele[1]))
-                    ele = cls.from_dict(ele[2])
-                    obj[key] = ele
+    for key, ele in obj.items():
+        if key == ARRAY_PREFIX:
+            return np.array(ele)
+        elif key == DTYPE_PREFIX:
+            return np.dtype(ele)
+        elif key == PINT_PREFIX:
+            return UREG(ele).units
+        elif key == FUNC_PREFIX:
+            return pickle.loads(eval(ele))
+        elif key == SERIES_PREFIX:
+            return pd.Series(ele)
+        elif key == DFRAME_PREFIX:
+            return pd.DataFrame(ele)
+        elif key == QUANT_PREFIX:
+            return ele[0] * UREG(ele[1]).units
+        elif key == DR_PREFIX or key == DICTABLE_PREFIX:
+            cls = pickle.loads(eval(ele[0]))
+            return cls.from_dict(ele[1])
 
     return obj
