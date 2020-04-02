85b9e9c039ffbf7600166583400a09ee11f957e9
gucky92 <gucky@gucky.eu>
2020-04-02 20:26:44 +0200

    notebooks change

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## DR(osophila)EYE: color vision package for model organisms
 
 Dreye is still being actively developed and tested, and documentation will be added soon.
+
+## Installation
+
+After cloning the repository pip install dreye:
+```
+pip install dreye
+```
--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 from setuptools import setup
 
-setup(name='dreye',
-      version='0.1',
-      description='Dreye',
-      author='Matthias Christenson',
-      author_email='gucky@gucky.eu',
-      install_requires=['numpy'],
-      packages=['dreye']
+setup(
+    name='dreye',
+    version='0.1',
+    description='Dreye',
+    author='Matthias Christenson',
+    author_email='gucky@gucky.eu',
+    install_requires=['numpy'],
+    # TODO requirement file
+    packages=['dreye']
 )
