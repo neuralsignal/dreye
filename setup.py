b52afc057488ce65ecdedd7c9b7ae1f331176d36
gucky92 <gucky@gucky.eu>
2020-06-17 20:20:56 +0200

    new setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 import setuptools
+import os
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(path, 'requirements.txt')) as f:
+    requirements = f.read().split()
 
 setuptools.setup(
     name='dreye',
@@ -10,5 +16,6 @@ setuptools.setup(
     author_email='gucky@gucky.eu',
     # install_requires=['requirements.txt'],
     # TODO requirement file
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(exclude=['tests', 'docs']),
+    install_requires=requirements
 )
